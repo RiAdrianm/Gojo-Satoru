ad64e0c630300e96fbd6f8d066d2a6050569aec6
RiAdrianm <103224046+RiAdrianm@users.noreply.github.com>
2022-09-30 22:40:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,32 +19,32 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.owner = ['6281271139724']
+global.premium = ['6281271139724']
+global.ownernomer = '6281271139724'
+global.ownername = 'Ri'
+global.botname = 'Chino'
+global.footer = 'Chino'
+global.ig = 'https://twitter.com/Riadrianm'
+global.region = 'Indonesia'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 't.me/LewdRi'
+global.packname = 'BPM Official'
+global.author = 'BPM Official'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'Wait A minute...',
-    error: 'Error! Maybe Api Key Is Expired🤔!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    success: 'Yatta Onii-chan ✓',
+    admin: 'Cuma *Admin* yang bisa ngelakuin itu!',
+    botAdmin: 'Aku harus jadi *Admin* dulu!',
+    owner: 'Fitur ini hanya untuk *Ri Onii-chan*',
+    group: 'Fitur ini hanya untuk *Group*!',
+    private: 'Fitur ini hanya untuk *Chat Pribadi*!',
+    bot: 'Fitur ini hanya untuk *Watashi* dake dayo!',
+    wait: '*Cotto Matte Onii-Chan*',
+    error: '*Watashi* koware teru!(Eror bang:v)',
+    endLimit: 'Kamu sudah mencapai *Limit* Onii-chan, tunggu *12jam* lagi yaa!',
 }
     global.limitawal = {
     premium: "Infinity",
